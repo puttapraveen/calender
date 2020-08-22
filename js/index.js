ea4fbc9f9c7d9b014dbb0996c0f00b3be49e2bde
puttapraveen <p.praveen.kumar.21.pk@gmail.com>
2020-08-22 23:25:38 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+  $(".datepicker").datepicker({
+    nextText: '<i class="fa fa-fw fa-angle-right"></i>',
+    prevText: '<i class="fa fa-fw fa-angle-left"></i>'
+  });
+});
